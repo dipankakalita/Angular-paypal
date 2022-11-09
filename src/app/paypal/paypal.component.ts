6bb0ae205d29a53c19c16cfef860321ff33f6bf3
Dipanka <dipankakalita2018@gmail.com>
2022-11-10 01:10:01 +0530

    paypal

--- src/app/paypal/paypal.component.ts
+++ src/app/paypal/paypal.component.ts
@@ -20,16 +20,59 @@ export class PaypalComponent implements OnInit {
   }
 
   private initConfig(): void {
-    this.payPalConfig = {
+    // this.payPalConfig = {
+    // currency: 'USD',
+    // clientId: 'AVQ6jFUeUKzsyR2p7L9TS4fz49-8TYYupo4wJfhuVOrwUP6VHWXuaABPRkBuMzcUQjUwuOCzCO53e0KY',
+    // createOrderOnClient: (data) => <ICreateOrderRequest>{
+    //   intent: 'CAPTURE',
+    //   purchase_units: [
+    //     {
+    //       amount: {
+    //         currency_code: 'USD',
+    //         value: '20.00'
+    //       }
+    //     }
+    //   ]
+    // },
+    // advanced: {
+    //   commit: 'true'
+    // },
+    // style: {
+    //   label: 'paypal',
+    //   layout: 'vertical'
+    // },
+    // onApprove: (data, actions) => {
+    //   console.log('onApprove - transaction was approved, but not authorized', data, actions);
+    //   actions.order.get().then((details: any) => {
+    //     console.log('onApprove - you can get full order details inside onApprove: ', details);
+    //     this.test()
+    //   });
+    // },
+    // onClientAuthorization: (data) => {
+    //   console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
+    //   this.showSuccess = true;
+    // },
+    // onCancel: (data, actions) => {
+    //   console.log('OnCancel', data, actions);
+    // },
+    // onError: err => {
+    //   console.log('OnError', err);
+    // },
+    // onClick: (data, actions) => {
+    //   console.log('onClick', data, actions);
+    // },
+  // };
+
+  this.payPalConfig = {
     currency: 'USD',
-    clientId: 'AVQ6jFUeUKzsyR2p7L9TS4fz49-8TYYupo4wJfhuVOrwUP6VHWXuaABPRkBuMzcUQjUwuOCzCO53e0KY',
+    clientId: 'AX2MNbOSgC95hEWIAps7L94ZIg48YdS5CSpicZTGlnPg3Z_Ydl5Cblv4oO2TbUDa1S9YLzeSTkNg31zS',
     createOrderOnClient: (data) => <ICreateOrderRequest>{
       intent: 'CAPTURE',
       purchase_units: [
         {
           amount: {
             currency_code: 'USD',
-            value: '20.00'
+            value: '10.00'
           }
         }
       ]
@@ -39,29 +82,31 @@ export class PaypalComponent implements OnInit {
     },
     style: {
       label: 'paypal',
-      layout: 'vertical'
+      layout: 'horizontal',
+      tagline :false,
     },
-    onApprove: (data, actions) => {
-      console.log('onApprove - transaction was approved, but not authorized', data, actions);
-      actions.order.get().then((details: any) => {
-        console.log('onApprove - you can get full order details inside onApprove: ', details);
-        this.test()
-      });
-    },
+    // onApprove: (data, actions) => {
+    //   console.log('onApprove - transaction was approved, but not authorized', data, actions);
+    //   actions.order.get().then((details: any) => {
+    //     console.log('onApprove - you can get full order details inside onApprove: ', details);
+    //   });
+    // },
+
     onClientAuthorization: (data) => {
       console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
-      this.showSuccess = true;
+      // this.paypalPayment()
     },
     onCancel: (data, actions) => {
-      console.log('OnCancel', data, actions);
+      // this.toasty.error(this.translate.instant('The payment has been cancelled, please try again!'));
+      // return this.router.navigate(['/payments/cancel']);
     },
     onError: err => {
-      console.log('OnError', err);
+      // return this.toasty.error(this.translate.instant(err));
     },
-    onClick: (data, actions) => {
-      console.log('onClick', data, actions);
-    },
-  };
+    // onClick: (data, actions) => {
+    //   console.log('onClick', data, actions);
+    // },
+  }
   }
 
   test(){
